6e0368ce260f99e8424f89084b37cbee51c2d9e5
chubbard91 <chubbard91@gmail.com>
2021-04-06 00:22:17 +0000

    first commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// // Array of user objects
+
+// var objUser = [
+//     {
+//         username: "test",
+//         password: "test"
+//     },
+//     {
+//         username: "",
+//         password: ""
+//     }
+// ]
+
+// function getInfo() {
+//     var username = document.getElementById("username").value
+//     var passward = document.getElementById("password").value
+
+//     for (i = 0; i < objPeople.length; i++) {
+//         if (username == objUser[i].username && password == objPeople[i].password) {
+//             //Map to Client Dashboard
+//             console.log(username + "welcome back")
+//             return;
+//         }
+//         else {
+//             console.log("Uh oh, looks like there was an error. Please try again.")
+//         }
+//         return;
+//     }
+// }
